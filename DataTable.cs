f20d9704aec7d84b7c7f9e714bc1badb9e0738ff
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-03-02 22:36:14 +0100

    1.0.3.0

--- DataTable.cs
+++ DataTable.cs
@@ -52,40 +52,53 @@ namespace SVN.Console2
             Console.WriteLine(this);
         }
 
-        private string DrawFullRow()
+        private string DrawColumnWhiteSpaces()
         {
-            return $"##{this.Lengths.Select(x => Enumerable.Range(1, x).Select(y => "#").Join("")).Join("###")}##";
+            return Enumerable.Range(1, 3).Select(x => " ").Join("");
         }
 
-        private string DrawEmptyRow()
+        private string DrawRowEmpty()
         {
-            return $"# {this.Lengths.Select(x => Enumerable.Range(1, x).Select(y => " ").Join("")).Join(" # ")} #";
+            return $"{this.DrawColumnWhiteSpaces()}  {this.Lengths.Select(x => Enumerable.Range(1, x).Select(y => " ").Join("")).Join("   ")}  {this.DrawColumnWhiteSpaces()}";
+        }
+
+        private string DrawRowWall()
+        {
+            return $"{this.DrawColumnWhiteSpaces()}##{this.Lengths.Select(x => Enumerable.Range(1, x).Select(y => "#").Join("")).Join("###")}##{this.DrawColumnWhiteSpaces()}";
+        }
+
+        private string DrawRowInputless()
+        {
+            return $"{this.DrawColumnWhiteSpaces()}# {this.Lengths.Select(x => Enumerable.Range(1, x).Select(y => " ").Join("")).Join(" # ")} #{this.DrawColumnWhiteSpaces()}";
         }
 
         private string DrawRow(DataRow row)
         {
-            return $"# {row.ToString(this.Lengths)} #";
+            return $"{this.DrawColumnWhiteSpaces()}# {row.ToString(this.Lengths)} #{this.DrawColumnWhiteSpaces()}";
         }
 
         private IEnumerable<string> DrawRows()
         {
+            yield return this.DrawRowEmpty();
             foreach (var row in this.Rows)
             {
-                yield return this.DrawFullRow();
-                yield return this.DrawEmptyRow();
+                yield return this.DrawRowWall();
+                yield return this.DrawRowInputless();
                 yield return this.DrawRow(row);
-                yield return this.DrawEmptyRow();
+                yield return this.DrawRowInputless();
             }
-            yield return this.DrawFullRow();
+            yield return this.DrawRowWall();
+            yield return this.DrawRowEmpty();
         }
 
         public override string ToString()
         {
             var rows = this.DrawRows().ToList();
             var maxRowLength = rows.Max(x => x.Length);
+            var scaling = 1.005;
 
-            var width = (maxRowLength * 1.01).CeilingToInt();
-            var height = (rows.Count * 1.01).CeilingToInt();
+            var width = (maxRowLength * scaling).CeilingToInt();
+            var height = (rows.Count * scaling).CeilingToInt();
 
             width = Math.Max(width, Console.WindowWidth);
             width = Math.Min(width, 240);
